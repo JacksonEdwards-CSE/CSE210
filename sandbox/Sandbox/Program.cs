504d01dc6c1fea62a542c42d81f0cdc5d41309c9
JacksonEdwards-CSE <jacksoned0713@gmail.com>
2026-03-02 10:00:16 -0700

    Practiced making classes

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
+        Circle myCircle = new Circle();
+
+        myCircle.SetRadius(10);
+
+        Console.WriteLine(myCircle.GetRadius());
     }
 }
