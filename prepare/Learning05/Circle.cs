5518ade482e6f9cded7e804a9b8559e02374d3f8
JacksonEdwards-CSE <jacksoned0713@gmail.com>
2026-03-23 20:41:47 -0600

    Prep 5

---
+++ prepare/Learning05/Circle.cs
@@ -0,0 +1,17 @@
+
+class Circle : Shape
+{
+    
+    private double _radius;
+
+    public Circle(double radius, string color)
+    : base(color)
+    {
+        _radius = radius;
+    }
+
+    public override double GetArea()
+    {
+        return Math.PI * _radius * _radius;
+    }
+}
--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+
+        List<Shape> shapeList = new List<Shape>();
+
+        Circle circle = new Circle(15, "blue");
+        Rectangle rectangle = new Rectangle(5, 10, "red");
+        Square square = new Square(5, "green");
+
+        shapeList.Add(circle);
+        shapeList.Add(rectangle);
+        shapeList.Add(square);
+
+        foreach (Shape shape in shapeList)
+        {
+            Console.WriteLine(shape.GetColor());
+            Console.WriteLine(shape.GetArea());
+        }
     }
 }
